04cbea67c566cb47f0e9406b22b707b3c9b00271
zx333445 <56999845+zx333445@users.noreply.github.com>
2023-09-18 09:58:12 +0800

    Update model_without_fpn.py

--- network/model_without_fpn.py
+++ network/model_without_fpn.py
@@ -15,20 +15,20 @@ def create_dense_model(num_classes, backbone_name, pretrained):
     backbone = densenet.__dict__[backbone_name](
         # weights=models.DenseNet169_Weights.DEFAULT   
         pretrained = pretrained    
-    ).features   # .features即只选取分类器之前的模型结构,去掉拉平的全连接层,分类器的序列名称为classifiers
+    ).features   
     
     for name, param in backbone.named_parameters():
         if "denseblock" not in name and "transition" not in name:
             param.requires_grad_(False)
 
-    backbone.out_channels = 1664  # 设置对应backbone输出特征矩阵的channels
+    backbone.out_channels = 1664  
 
     anchor_generator = AnchorsGenerator(sizes=((32, 64, 128, 256, 512),),
                                         aspect_ratios=((0.5, 1.0, 2.0),))
 
-    roi_pooler = MultiScaleRoIAlign(featmap_names=['0'],  # 在哪些特征层上进行roi pooling
-                                    output_size=[7, 7],   # roi_pooling输出特征矩阵尺寸
-                                    sampling_ratio=2)  # 采样率
+    roi_pooler = MultiScaleRoIAlign(featmap_names=['0'],  
+                                    output_size=[7, 7],   
+                                    sampling_ratio=2)  
 
     model = CascadeMiningDet(backbone=backbone,
                        num_classes=num_classes,
